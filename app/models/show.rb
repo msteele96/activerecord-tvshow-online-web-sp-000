ad2b5d861909417b2be090e2512ddf43caf6f44e
Marc Steele <marcsteele2014@gmail.com>
2021-01-25 21:08:58 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,11 +5,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-
+    self.maximum("rating")
   end
 
   def self.lowest_rating
-
+    self.minimum("rating")
   end
 
   def self.least_popular_show
