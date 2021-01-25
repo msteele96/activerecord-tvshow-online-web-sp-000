0b76befa218d6722f927631c0c05640024d23870
Marc Steele <marcsteele2014@gmail.com>
2021-01-25 21:13:27 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,8 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.order("rating DESC").limit(1)
+    # self.order("rating DESC").limit(1)
+    self.where("rating = ?", self.highest_rating).first
   end
 
   def self.lowest_rating
